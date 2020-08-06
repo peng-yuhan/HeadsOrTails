f4fbaafe338e55d9c2f8f0b74d1e3af851c76d95
peng-yuhan <69300967+peng-yuhan@users.noreply.github.com>
2020-08-06 22:07:57 +0900

    HoT:Initial revision

---
+++ HeadsOrTails.java
@@ -0,0 +1,35 @@
+import java.util.Random;
+
+class Toss{
+	public String result() {
+		Random r = new Random();
+		int value = r.nextInt(2);
+		if(value == 1) {
+			return "Heads";
+		}
+		else {
+			return "Tails";
+		}
+	}
+}
+
+public class HeadsOrTails{
+	public static void main(String []args) {
+		int heads = 0;
+		int tails = 0;
+		int round = 3;
+		String h = "Heads";
+		System.out.println("Tossing a coin...\n");
+		Toss t = new Toss();
+		for(int i=1; i<=round; i++) {
+			String n = t.result();
+			System.out.println("Round " + i + ": " + n + "\n");
+			if(n.equals(h)) {
+				heads++;
+			}
+			else {
+				tails++;}
+		}
+		System.out.println("\nHeads: " + heads + ", Tails: " + tails);
+	}
+}
